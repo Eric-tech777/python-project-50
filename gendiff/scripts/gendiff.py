9742e19f380177f50b8f97825b21e770cf9f03ec
Irken Ivaev <ofs.iri@gmail.com>
2024-03-08 16:58:35 +0300

    update files of project2 on step2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,17 +2,15 @@
 import argparse
 import json
 
-
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument("-f ", "--format", help='set format of output')
     args = parser.parse_args()
-    # print(args.first_file)
-    # print(args.second_file)
+    generate_diff(args.first_file, args.second_file)
     
-'''
+
 def generate_diff(path1, path2):
     dict1 = json.load(open(path1))
     sorted_list1 = sorted(dict1.items(), key=lambda x: x[0])
@@ -50,15 +48,8 @@ def generate_diff(path1, path2):
 
     result_string1 = [f'  {get_mark(item)} {item[0]}: {str(item[1]).lower()}\n' for item in sorted_list3]
     
-    return f'{{\n{''.join(result_string1)}}}'
+    print(f"{{\n{''.join(result_string1)}}}")
 
-
-p1 = 'file1.json'
-p2 = 'file2.json'
-
-print(generate_diff(p1, p2))
-'''
 
 if __name__ == "__main__":
     main()
-
