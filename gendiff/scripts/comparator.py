f808995e2f7108deac1f82052f8157ef7e561276
Irken Ivaev <ofs.iri@gmail.com>
2024-04-16 12:35:48 +0300

    upload files of project2 step6

--- gendiff/scripts/comparator.py
+++ gendiff/scripts/comparator.py
@@ -1,68 +1,65 @@
+import json
 
 
-def data_parser(list1, list2):
-    # начало кода сравнения
-    res_list = add_twins(list1, list2)
-    res1_list = add_same_keys(res_list, list1, list2)
-    res2_list = add_moved_from_file1(res1_list, list1)
-    res3_list = add_new_to_file2(res2_list, list2)
-    sorted_result_list = sort_result_list(res3_list)
-    result_string = tip_out(sorted_result_list, list1, list2)
+def data_parser(dict1, dict2):
+    data1 = comparator(dict1, dict2)
+    data2 = order_dict(data1)
+    result_string = stylish(data2, count_space=4)
+    print(result_string)
     return result_string
-    # завершение кода сравнения
 
 
-# начало кода обработки файлов и сравнения
-def add_twins(list1, list2):  # вносим в список одинаковые кортежи
-    res_list = []
-    [res_list.append(i) for i in list1 for j in list2 if i == j]
-    return res_list
+def comparator(dict1, dict2):
+    the_same = (list(dict1.keys() & dict2.keys()))
+    removed = (list(dict1.keys() - dict2.keys()))
+    added = (list(dict2.keys() - dict1.keys()))
+    result = {}
+    for i in the_same:
+        if dict1[i] == dict2[i]:
+            result["  " + i] = str(dict1[i])
+    for i in the_same:
+        if dict1[i] != dict2[i]:
+            if isinstance(dict1[i], dict) and isinstance(dict2[i], dict):
+                result["  " + i] = comparator(dict1[i], dict2[i])
+            else:
+                result["- " + i] = dict1[i]
+                result["+ " + i] = dict2[i]
+    for i in added:
+        result["+ " + i] = dict2[i]
+    for i in removed:
+        result["- " + i] = dict1[i]
+    return result
 
 
-# вносим в список кортежи с измененным значением
-def add_same_keys(res_list, list1, list2):
-    for i in list1:
-        for j in list2:
-            if i[0] == j[0] and i[1] != j[1]:
-                res_list.append(i)
-                res_list.append(j)
-
-    return res_list
-
-
-# вносим в список кортежи сo значениями удаленными из файла1
-def add_moved_from_file1(res1_list, list1):
-    [res1_list.append(i) for i in list1 if i in list1 and i not in res1_list]
-    return res1_list
+# Получение Отсортированного представления для Форматера
+def order_dict(d):
+    result = {}
+    for key, val in sorted(d.items(), key=lambda x: x[0].strip(" -+")):
+        if isinstance(val, dict):
+            result[key] = order_dict(val)
+        else:
+            result[key] = val
+    return result
 
 
-# вносим в список кортежи с новыми значениями (добавленными элементами)
-def add_new_to_file2(res2_list, list2):
-    [res2_list.append(j) for j in list2 if j in list2 and j not in res2_list]
-    return res2_list
-
-
-# сортировка списка элементов для формирования отчета
-def sort_result_list(result_list):
-    sorted_result_list = sorted(result_list, key=lambda elem: elem[0])
-    return sorted_result_list
-
-
-# начало формирования отчета по разнице файлов
-def tip_out(sorted_result_list, list1, list2):
-    def get_mark(item):  # получить знак статуса элементов (' ', '-', '+')
-        mark = '0'
-        if item in list1 and item in list2:
-            mark = ' '
-        if item in list1 and item not in list2:
-            mark = '-'
-        if item in list2 and item not in list1:
-            mark = '+'
-        return mark
-
-# генератор отчета по каждому элементу для отчета
-    string_to_join = [f'  {get_mark(item)} {item[0]}:\
- {str(item[1]).lower()}\n' for item in sorted_result_list]
-# сбор отчета для вывода на печать
-    result = f"{{\n{''.join(string_to_join)}}}"
+# Форматер stylish
+def stylish(data2, replacer=' ', count_space=1, rank=1):
+    if isinstance(data2, dict):
+        result = '{\n'
+        for elem, value in data2.items():
+            if elem[0:2] in ['+ ', '- ', '  ']:
+                some = 2
+            else:
+                some = 0
+            if value == 'False':
+                value = json.dumps(False)
+            if value == 'None':
+                value = json.dumps(None)
+            if value == 'True':
+                value = json.dumps(True)
+            result += f'{replacer * (count_space * rank - some)}{elem}: '
+            result += stylish(value, replacer, count_space, rank + 1) + '\n'
+        result += replacer * count_space * (rank - 1) + '}'
+    else:
+        result = str(data2)
     return result
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -22,12 +22,14 @@ def generate_diff(path_file1, path_file2):
     dict1, dict2 = '', ''
     if path_file1.endswith('json') and path_file2.endswith('json'):
         dict1, dict2 = make_json_dicts(path_file1, path_file2)
+
     if path_file1.endswith('yml') and path_file2.endswith('yml'):
         dict1, dict2 = make_yml_dicts(path_file1, path_file2)
+
     elif path_file1.endswith('yaml') and path_file2.endswith('yaml'):
         dict1, dict2 = make_yml_dicts(path_file1, path_file2)
-    list1, list2 = make_sorted_lists(dict1, dict2)
-    data_parser(list1, list2)
+
+    data_parser(dict1, dict2)
 
 
 # чтение файлов json
@@ -48,13 +50,6 @@ def make_yml_dicts(path1, path2):
         gen_obj2 = yaml.load_all(path_2, Loader=yaml.FullLoader)
         [dict2] = gen_obj2
     return dict1, dict2
-
-
-# получение отсортированных списков кортежей с данными из файла1 и файла2
-def make_sorted_lists(dict1, dict2):
-    sorted_list1 = sorted(dict1.items(), key=lambda x: x[0])
-    sorted_list2 = sorted(dict2.items(), key=lambda x: x[0])
-    return sorted_list1, sorted_list2
 
 
 if __name__ == "__main__":
