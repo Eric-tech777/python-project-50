e7fe18c9acff0e3c91b01e01a8154fd3daf04a02
Irken Ivaev <ofs.iri@gmail.com>
2024-06-29 17:24:48 +0300

    updated yml test file

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -9,9 +9,9 @@ import pytest
 ("jsonfile1.json", "jsonfile2.json", "stylish", "res_file_stylish.txt"),
 ("jsonfile1.json", "jsonfile2.json", "json", "res_file_json.txt"),
 ("jsonfile1.json", "jsonfile2.json", "plain", "res_file_plain.txt"),
-("yamlfile1.yaml", "yamlfile2.yaml", "stylish", "res_file_stylish.txt"),
+("ymlfile1.yml", "ymlfile2.yml", "stylish", "res_file_stylish.txt"),
 # ("yamlfile1.yaml", "yamlfile2.yaml", "plain", "res_file_plain.txt"),
-("yamlfile1.yaml", "yamlfile2.yaml", "json", "res_file_json.txt")])
+("ymlfile1.yml", "ymlfile2.yml", "json", "res_file_json.txt")])
 def test_generate_diff(file_path1, file_path2, format_name, result_file):
 	with (open(Path(__file__).parent / 'fixtures/' / result_file, 'r', encoding='utf-8') as result):
 		result_file = ast.literal_eval(result.read().strip())
